f8fea0deebef940c9cdeadc59e1d8ada43a5145a
Alicia Riggs <aliciariggs.15.ar@gmail.com>
2017-03-21 13:57:13 -0700

    update

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
 	$("#add").on('click', function(){
-		var num_1 = $("#value1").val()
-		var num_2 = $("#value2").val()
+		var num_1 = $("#num_1").val()
+		var num_2 = $("#num_2").val()
 		$("#answer").val(Number(num_1) + Number(num_2))
 	})
 })
 $(document).ready(function(){
 	$("#sub").on('click', function(){
-		var num_1 = $("#value1").val()
-		var num_2 = $("#value2").val()
+		var num_1 = $("#num_1").val()
+		var num_2 = $("#num_2").val()
 		$("#answer").val(Number(num_1) - Number(num_2))
 	})
 })
 $(document).ready(function(){
 	$("#div").on('click', function(){
-		var num_1 = $("#value1").val()
-		var num_2 = $("#value2").val()
+		var num_1 = $("#num_1").val()
+		var num_2 = $("#num_2").val()
 		$("#answer").val(Number(num_1) / Number(num_2))
 	})
 })
 $(document).ready(function(){
 	$("#multi").on('click', function(){
-		var num_1 = $("#value1").val()
-		var num_2 = $("#value2").val()
+		var num_1 = $("#num_1").val()
+		var num_2 = $("#num_2").val()
 		$("#answer").val(Number(num_1) * Number(num_2))
 	})
 })
